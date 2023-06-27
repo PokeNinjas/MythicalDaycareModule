1e9dae77e4b1cc7ab484c344ea4428a2d9bb38a8
Stampede <carterblowers01@gmail.com>
2023-06-27 10:44:39 -0500

    Updated arch

--- buildSrc/src/main/kotlin/com/mythicalnetwork/gradle/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/mythicalnetwork/gradle/Dependencies.kt
@@ -6,7 +6,7 @@ object Dependencies {
     const val QUILT_KOTLIN_LIBRARIES = "org.quiltmc.quilt-kotlin-libraries:library:${Versions.QUILT_KOTLIN_LIBRARIES}"
     const val QUILT_LOADER = "org.quiltmc:quilt-loader:${Versions.QUILT_LOADER}"
     const val QUILTED_FABRIC_API = "org.quiltmc.quilted-fabric-api:quilted-fabric-api:${Versions.QUILTED_FABRIC_API}-${Versions.MINECRAFT}"
-    const val ARCHITECTURY = "dev.architectury:architectury-fabric:6.5.69"
+    const val ARCHITECTURY = "dev.architectury:architectury-fabric:8.1.87"
     const val COBBLEMON = "com.cobblemon:fabric:1.3.1+1.19.2-SNAPSHOT"
     const val JUNIT_JUPITER_API = "org.junit.jupiter:junit-jupiter-api:5.9.2"
     const val JUNIT_JUPITER_ENGINE = "org.junit.jupiter:junit-jupiter-engine:5.9.2"
